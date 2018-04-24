17d880e2b6835f4165739cb4174a87d94248b975
ininmm <ininmm830120@gmail.com>
2018-04-24 23:49:16 +0800

    [fix] change and initial annotation default tags

--- library/src/main/java/com/github/ininmm/library/annotation/Produce.kt
+++ library/src/main/java/com/github/ininmm/library/annotation/Produce.kt
@@ -3,9 +3,10 @@ package com.github.ininmm.library.annotation
 import com.github.ininmm.library.thread.EventThread
 
 /**
+ * 用來註解需要 sticky send 的方法
  * Created by User
  * on 2018/2/11.
  */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-annotation class Produce(val thread: EventThread = EventThread.MainThread)
+annotation class Produce(val tags: Array<TagModel.Tag> = [], val thread: EventThread = EventThread.MainThread)
